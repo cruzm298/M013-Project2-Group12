9ebf597be7e27b27fe42e09f6939063e8d1bb8f0
winstontsui <91974001+winstontsui@users.noreply.github.com>
2021-10-30 19:04:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import random
 
+
+def exploreonly():
+    mysum = 0
+    for i in range(100): # Visit each cafeteria 100 times. Each time, add it's happiness value.
+        happinessCAFE1 = random.normalvariate(9, 3)
+        happinessCAFE2 = random.normalvariate(7, 5)
+        happinessCAFE3 = random.normalvariate(11, 7)
+        mysum = mysum + happinessCAFE1 + happinessCAFE2 + happinessCAFE3
+    print(mysum)
