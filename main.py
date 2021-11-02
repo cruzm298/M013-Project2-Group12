a2254101fd6546bc16cdce32ab583aa42d54473f
cruzm298 <86684032+cruzm298@users.noreply.github.com>
2021-11-02 12:11:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 
-
+# explore only
 def exploreonly():
     mysum = 0
     for i in range(100): # Visit each cafeteria 100 times. Each time, add it's happiness value.
@@ -9,3 +9,5 @@ def exploreonly():
         happinessCAFE3 = random.normalvariate(11, 7)
         mysum = mysum + happinessCAFE1 + happinessCAFE2 + happinessCAFE3
     print(mysum)
+    
+# exploit only
